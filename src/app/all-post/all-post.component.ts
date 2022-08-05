c31a63b2a78253cb3f0ad0e1ce2cad9ed29a8748
mitaligemini <Mitali.Varshney@Gemini.com>
2022-08-05 18:18:34 +0530

    updation in post upload

--- src/app/all-post/all-post.component.ts
+++ src/app/all-post/all-post.component.ts
@@ -13,7 +13,9 @@ export class AllPostComponent implements OnInit {
   message = "post uploaded"
 
   ngOnInit(): void {
-    this.allPosts = ["Aashi", "Megha", "Shivangi"]
+    this.allPosts = ["Google was founded on September 4, 1998, by Larry Page and Sergey Brin while they were PhD students at Stanford University in California. Together they own about 14% of its publicly listed shares and control 56% of the stockholder voting power through super-voting stock. ",
+      "Google strives for ambitious technological innovations aimed at solving humanity's biggest problems.[17] Some of these include quantum computing (Sycamore), self-driving cars (Waymo, formerly the Google Self-Driving Car Project), smart cities (Sidewalk Labs), and transformer models (Google Brain).",
+      "Google began in January 1996 as a research project by Larry Page and Sergey Brin when they were both PhD students at Stanford University in California.[21][22][23] The project initially involved an unofficial, Scott Hassan, the original lead programmer who wrote much of the code for the original Google Search engine, but he left before Google was officially founded as a company"]
     this.PostService.getValue().subscribe((value) => {
       if (value != '') {
         this.allPosts.push(value)
