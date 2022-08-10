bd6aa833a6fb7631566757202707c1a904a56b0e
mitaligemini <Mitali.Varshney@Gemini.com>
2022-08-10 14:23:06 +0530

    Integrated delete-post API

--- src/app/all-post/all-post.component.ts
+++ src/app/all-post/all-post.component.ts
@@ -1,5 +1,6 @@
+import { HttpService } from './../services/http.service';
 import { PostService } from '../services/post.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-all-post',
@@ -7,22 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-post.component.css']
 })
 export class AllPostComponent implements OnInit {
+  user_id: string = "";
 
-  constructor(private PostService: PostService) { }
+  constructor(private PostService: PostService, private http: HttpService) { }
   allPosts: any = [];
+  posts: any = []
   message = "post uploaded"
+  idOfPost:string=""
 
   ngOnInit(): void {
-    this.allPosts = ["Google was founded on September 4, 1998, by Larry Page and Sergey Brin while they were PhD students at Stanford University in California. Together they own about 14% of its publicly listed shares and control 56% of the stockholder voting power through super-voting stock. ",
-      "Google strives for ambitious technological innovations aimed at solving humanity's biggest problems.[17] Some of these include quantum computing (Sycamore), self-driving cars (Waymo, formerly the Google Self-Driving Car Project), smart cities (Sidewalk Labs), and transformer models (Google Brain).",
-      "Google began in January 1996 as a research project by Larry Page and Sergey Brin when they were both PhD students at Stanford University in California.[21][22][23] The project initially involved an unofficial, Scott Hassan, the original lead programmer who wrote much of the code for the original Google Search engine, but he left before Google was officially founded as a company"]
-    this.PostService.getValue().subscribe((value) => {
-      if (value != '') {
-        this.allPosts.push(value)
-        console.log("post posted")
-      }
-    });
+    this.user_id = localStorage.getItem('user_id')!;
+    // console.log(this.user_id)
+    // this.allPosts = ["Google was founded on September 4, 1998, by Larry Page and Sergey Brin while they were PhD students at Stanford University in California. Together they own about 14% of its publicly listed shares and control 56% of the stockholder voting power through super-voting stock. ",
+    //   "Google strives for ambitious technological innovations aimed at solving humanity's biggest problems.[17] Some of these include quantum computing (Sycamore), self-driving cars (Waymo, formerly the Google Self-Driving Car Project), smart cities (Sidewalk Labs), and transformer models (Google Brain).",
+    //   "Google began in January 1996 as a research project by Larry Page and Sergey Brin when they were both PhD students at Stanford University in California.[21][22][23] The project initially involved an unofficial, Scott Hassan, the original lead programmer who wrote much of the code for the original Google Search engine, but he left before Google was officially founded as a company"]
+
+    this.http.post('/post/get-all-post', { _userId: this.user_id }).subscribe((res) => {
+      //console.log("allposts", res)
+      this.posts = res.posts;
+      console.log("posts",this.posts)
+    })
+  }
+
+  deletePost(event:any){
+    console.log(event)
+     this.http.post(`/post/delete/${event}`,{_userId:this.user_id}).subscribe((res)=>{
+      console.log(res);
+    })
+    alert("Post deleted successfully")
+    location.reload()
   }
 }
-
 
