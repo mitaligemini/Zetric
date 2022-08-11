651fc668879da7c4c979409abe7821062a36d135
mitaligemini <Mitali.Varshney@Gemini.com>
2022-08-11 17:35:39 +0530

    dropdown created

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -11,6 +11,19 @@ export class NavbarComponent implements OnInit {
   constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
+    document.getElementById("drop")!.addEventListener("click", function(e) {
+      document.getElementById("myDropdown")!.classList.toggle("show");
+    });
+    
+    
+    // Close the dropdown if the user clicks outside of it
+    window.addEventListener("click", function(event ) {
+      if (!(event.target as HTMLInputElement).matches('.dropbtn')) {
+        document.querySelectorAll(".dropdown-content.show")
+          .forEach(openDropdown => openDropdown.classList.remove('show'))
+      }
+    });
+    
   }
 
   logout(){
